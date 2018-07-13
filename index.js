098dd907130faa634baed9c3688cd9239741cab0
veronicacheung1105 <veronica.cheung1105@gmail.com>
2018-07-13 23:29:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,10 +1,7 @@
 // Write your solution here!
 const drivers = ["Milo", "Otis", "Garfield"]
 
-function destructivelyAppendDriver(drivers){
-  drivers.concat('Ralph')
-  return drivers
-}
+
 
 drivers.push('Ralph')
 
